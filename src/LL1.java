4517c6db09c50d7273ee8db6ed88f82ed1bba954
Márta Krisztián <martakrisztian97@gmail.com>
2022-03-13 14:02:35 +0100

    Create Rule class

--- src/LL1.java
+++ src/LL1.java
@@ -1,6 +1,43 @@
+import java.util.*;
+
 public class LL1 {
 
-    public static void main(String[] args) {
+    /**
+     * Szabalyok tarolasa szolgalo osztaly.
+     */
+    public static class Rule {
+        private char left;    // szabaly bal oldala
+        private String right; // szabaly jobb oldala
+        private int serial;   // szabaly sorszama
 
+        public Rule(char left, String right, int serial) {
+            this.left = left;
+            this.right = right;
+            this.serial = serial;
+        }
+
+        public char getLeft() {
+            return left;
+        }
+
+        public String getRight() {
+            return right;
+        }
+
+        public int getSerial() {
+            return serial;
+        }
+
+        @Override
+        public String toString() {
+            return "Rule{" +
+                    "left=" + left +
+                    ", right='" + right + '\'' +
+                    ", serial=" + serial +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
     }
 }
