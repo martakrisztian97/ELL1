87942cf84643624bf92f9ce08f9b8c8d9657c786
Márta Krisztián <martakrisztian97@gmail.com>
2022-03-13 15:23:32 +0100

    Create global variables and methods getTerminals(), getNonTerminals(), isTerminal()

--- src/LL1.java
+++ src/LL1.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 
 public class LL1 {
@@ -35,6 +37,51 @@ public class LL1 {
                     ", right='" + right + '\'' +
                     ", serial=" + serial +
                     '}';
+        }
+    }
+
+    /**
+     * Globalis valtozok:
+     * - Szabalyokat tartalmazo lista
+     * - Terminalisok gyujtemenye
+     * - Nem terminalisok gyujtemenye
+     */
+    public static final List<Rule> rules = new ArrayList<>(Arrays.asList(
+            new Rule('S', "aS", 1), new Rule('S', "bAc", 2),
+            new Rule('A', "bAc", 3), new Rule('A', "d", 4)
+    ));
+    public static final Set<Character> terminals = new TreeSet<>();
+    public static final Set<Character> nonTerminals = new TreeSet<>();
+
+    /**
+     * Megvizsgal egy karaktert, hogy az terminalis-e.
+     * @param c A vizsgalt karakter.
+     * @return Azzal ter vissza, hogy a vizsgalt karakter terminalis-e.
+     */
+    public static boolean isTerminal(char c) {
+        boolean terminal = (c >= 'a' && c <= 'z') ? true : false;
+        return terminal;
+    }
+
+    /**
+     * Nem terminalisok gyujtemenyenek feltoltese.
+     */
+    public static void getNonTerminals() {
+        for (Rule r : rules) {
+            nonTerminals.add(r.left);
+        }
+    }
+
+    /**
+     * Terminalisok gyujtemenyenek feltoltese.
+     */
+    public static void getTerminals() {
+        for (Rule r : rules) {
+            for (int i = 0; i < r.right.length(); i++) {
+                if (isTerminal(r.right.charAt(i))) {
+                    terminals.add(r.right.charAt(i));
+                }
+            }
         }
     }
 
